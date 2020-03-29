bf2422e85a58742204840fb69dca0aa6111d3c96
annaPanda8170 <handa_ryohei@yahoo.co.jp>
2020-03-29 13:32:23 +0900

    tableからdivに変更

--- app/assets/javascripts/myinfos.js
+++ app/assets/javascripts/myinfos.js
@@ -1,3 +1,34 @@
-$(function () {
- 
+// $(function() {
+//   $(".draggable").draggable({ revert: "invalid", snap: ".table", snapMode: 'inner', }).droppable({});
+//   $(".table").droppable({
+//     drop: function (e, ui) {
+//       let $srcObj = $(ui.draggable[0]);
+//       $srcObj.offset($(this).offset());
+//       return false;
+//     },
+//   });
+// });
+
+function modDroparea(){
+  $('.table').each(function () {
+    $(this).droppable({
+      hoverClass: 'imgover',
+      disabled: $(this).find('.draggable').length > 0
+    });
+  });
+}
+
+
+$(function(){
+  $('.draggable').draggable({
+    revert:'invalid',
+    zIndex:'1000',
+  });
+  modDroparea();
+  $('.table').droppable({
+      drop: function(e, ui){
+        ui.draggable.prependTo(this).css({top:'0',left:'0'});
+        modDroparea();
+      },
+    });
 });
--- app/assets/javascripts/reservations.js
+++ app/assets/javascripts/reservations.js
@@ -1,3 +1,24 @@
+function allTable(){
+  $('.rsvTable__table').each(function () {
+    $(this).droppable({
+      hoverClass: 'dropHover',
+      disabled: $(this).find('.reservationOne').length > 0
+    });
+  });
+}
+
 $(function () {
-  $(".reservationOne").draggable()
+  allTable()
+  $(".reservationOne").draggable({
+    revert: "invalid",
+    zIndex: "100"
+  });
+  $(".rsvTable__table").droppable({
+    // ドロップ要素にドロップされた eがイベント自体?uiはドラッグしたことに関する情報
+    drop: function (e, ui) {
+      console.log(e)
+      ui.draggable.prependTo(this).css({ top: '0', left: '0' });
+      allTable()
+    }
+  })
 })

26fe167ab4602dfe1169aab0998d9c54f2fe62e2
annaPanda8170 <handa_ryohei@yahoo.co.jp>
2020-03-30 02:07:17 +0900

    draggable用調整

--- app/assets/javascripts/reservations.js
+++ app/assets/javascripts/reservations.js
@@ -1,24 +1,39 @@
-function allTable(){
-  $('.rsvTable__table').each(function () {
+function allTable(aaaaaa){
+  $('.rsvTableDroppable').each(function () {
     $(this).droppable({
       hoverClass: 'dropHover',
       disabled: $(this).find('.reservationOne').length > 0
     });
+    // console.log(this)
   });
+  // console.log(aaaaaa)
+  
 }
-
+let aaaaaa;
 $(function () {
   allTable()
   $(".reservationOne").draggable({
     revert: "invalid",
-    zIndex: "100"
-  });
-  $(".rsvTable__table").droppable({
-    // ドロップ要素にドロップされた eがイベント自体?uiはドラッグしたことに関する情報
-    drop: function (e, ui) {
-      console.log(e)
-      ui.draggable.prependTo(this).css({ top: '0', left: '0' });
-      allTable()
+    drag: function () {
+      console.log(aaaaaa)
+      
     }
-  })
-})
+  });
+  $(".rsvTableDroppable").droppable({
+    drop: function (e, ui) {
+      ui.draggable.prependTo(this).css({ top: '0', left: '0' });
+      // console.log(e.target.dataset.set);
+      aaaaaa = Number(e.target.dataset.set) + 1;
+      allTable(aaaaaa)
+    },
+    classes: function () {
+      $("[data-set=" + aaaaaa + "]").removeClass("rsvTableDroppable");
+      $("[data-set=" + aaaaaa + "]").removeClass("ui-droppable");
+      $("[data-set=" + aaaaaa + "]").removeClass("ui-droppable-hover");
+      $("[data-set=" + aaaaaa + "]").removeClass("ui-droppable-active");
+    }
+    // over: function () {
+      
+    // }
+  });
+});

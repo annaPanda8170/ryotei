d711230eb8f879f464e208a72c0b357504200e98
annaPanda8170 <handa_ryohei@yahoo.co.jp>
2020-03-31 22:09:29 +0900

    重なった予約ができないように＊
    未完事項
    ・一度bookedバリデーションにかけた後に同じ部屋で1時間ずらしたりした時になぜかprependToの先がおかしくなる（dataの値もデータベースの値もおかしくないのに）

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_27_184558) do
+ActiveRecord::Schema.define(version: 2020_03_31_115039) do
 
   create_table "clients", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -55,7 +55,7 @@ ActiveRecord::Schema.define(version: 2020_03_27_184558) do
     t.bigint "room_id"
     t.bigint "kaiseki_id"
     t.integer "number_of_guest"
-    t.time "start_time", null: false
+    t.integer "start_time", null: false
     t.text "memo"
     t.date "date"
     t.integer "status", default: 1

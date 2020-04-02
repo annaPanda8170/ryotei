1bdcd1fb7965ea3c3e8c1b0d8637deebab13e4a0
annaPanda8170 <handa_ryohei@yahoo.co.jp>
2020-04-03 01:46:43 +0900

    clients#showにreservations#showのリンク

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -4,6 +4,7 @@ class ClientsController < ApplicationController
   end
   def show
     @client = Client.find(params[:id])
+    @reservation = Reservation.where(client_id: @client.id)
   end
   def new
     @client = Client.new

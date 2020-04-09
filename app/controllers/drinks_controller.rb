80172edbfda039aaca977bfb5b7016e9f8d78e9f
annaPanda8170 <handa_ryohei@yahoo.co.jp>
2020-04-09 23:38:55 +0900

    drinks#index

--- app/controllers/drinks_controller.rb
+++ app/controllers/drinks_controller.rb
@@ -3,6 +3,9 @@ class DrinksController < ApplicationController
   def index
     @drinks = Drink.all
   end
+  def show
+    @drink = Drink.find(params[:id])
+  end
   def new
     @drink = Drink.new
     @drink_price = 10

22337f9b5a454f9414f7bf5245eb4ed8e7c3b09b
annaPanda8170 <handa_ryohei@yahoo.co.jp>
2020-04-10 14:47:08 +0900

    reservationモデルのテスト

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,11 +1,16 @@
 class Reservation < ApplicationRecord
-  validates :guest, presence: true, unless: :client_id?
+  # もともとunless: :client_id?と書いていたが、テストでうまくいかないのでこれに変更した
+  validates :guest, presence: true, unless: :client
   # 以下なぜか二重にでてしまうのでコメントアウト
   # validates :room, presence: true
   # validates :kaiseki, presence: true
   validates :number_of_guest, presence: true
   validates :date, presence: true
   validates :start_hour, presence: true
+  validate :opening_hour
+  validates :start_minute, presence: true
+  validate :four_minute
+  validate :out_of_opening
   validate :booked
 
   belongs_to :client, optional: true
@@ -44,6 +49,21 @@ class Reservation < ApplicationRecord
     end
     if book
       errors.add(:date, "すでに予約があります")
+    end
+  end
+  def opening_hour
+    if start_hour < 11 || 19 < start_hour
+      errors.add(:start_hour, "時間外です")
+    end
+  end
+  def four_minute
+    if !(start_minute == 0 || start_minute == 15 || start_minute == 30 || start_minute == 45)
+      errors.add(:start_minute, "指定できない時間です")
+    end
+  end
+  def out_of_opening
+    if start_hour == 19 || start_minute == 45
+      errors.add(:start_hour, "時間外です")
     end
   end
 end
---
+++ spec/factories/kaisekis.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :kaiseki do
+    name {"高砂会席"}
+    price {20000}
+  end
+end
